7f18ddadcaea32a81288be58e302984267c43369
Deep Bratt Mondal <deepbratt@gmail.com>
2021-04-05 02:04:05 +0530

    optimised styling

--- src/Components/Layout/index.js
+++ src/Components/Layout/index.js
@@ -1,11 +1,24 @@
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
 import PropTypes from "prop-types";
 import React from "react";
+import LayoutStyle from "./style";
+
 const Layout = ({ children }) => {
+  const { root, paper } = LayoutStyle();
   return (
-    <div className="container">
-      <div>Header</div>
-      <div>{children}</div>
-      <div>footer</div>
+    <div className={root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper className={paper}>Header</Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper className={paper}>{children}</Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper className={paper}>Footer</Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 };
