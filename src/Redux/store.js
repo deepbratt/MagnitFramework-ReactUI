69b799e159989fe99537bb1883da0c7eac868cb7
ameera-saleem <ameerasaleem430@gmail.com>
2021-04-10 19:20:54 -0700

    Modified store with redux-persist lib

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,23 +1,25 @@
 import { applyMiddleware, compose, createStore } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { createLogger } from "redux-logger";
 import thunk from "redux-thunk";
 import rootReducer from "./Reducers/rootReducer";
-import { loadState, saveState } from "../Redux/localStorage";
+
+const persistConfig = {
+  key: "LIGHT",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleware = [thunk, createLogger()];
-const persistedState = loadState();
-const store = createStore(
-  rootReducer,
-  persistedState,
+
+export const store = createStore(
+  persistedReducer,
   compose(
     applyMiddleware(...middleware)
     // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
-// To persist state and restore it after refresh
-store.subscribe(() => {
-  saveState(store.getState());
-});
-
-export default store;
+export const persistor = persistStore(store);
