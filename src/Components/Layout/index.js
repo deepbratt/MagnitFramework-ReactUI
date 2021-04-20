d0fbcad36e6020eb3a5827b6ef9913dc5d21d7fb
deepbratt <deepbratt@gmail.com>
2021-04-21 00:23:34 +0530

    removing layout header and footer

--- src/Components/Layout/index.js
+++ src/Components/Layout/index.js
@@ -1,12 +1,10 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import PropTypes from "prop-types";
+import React from "react";
+import { useSelector } from "react-redux";
 import LayoutStyle from "./style";
-import Container from "@material-ui/core/Container";
-import LayoutSwiper from "../LayoutSwiper";
-import Switch from "../themeToggle/Switch";
 
 const Layout = ({ children }) => {
   const { root, paper } = LayoutStyle();
@@ -15,19 +13,19 @@ const Layout = ({ children }) => {
     <div className={root}>
       <Container maxWidth={layoutType} disableGutters={true}>
         <Grid container spacing={3}>
-          <Grid item xs={12}>
+          {/* <Grid item xs={12}>
             <Paper className={paper}>Header</Paper>
-          </Grid>
+          </Grid> */}
           <Grid item xs={12}>
             <Paper className={paper}>{children}</Paper>
           </Grid>
-          <Grid item xs={12}>
+          {/* <Grid item xs={12}>
             <Paper className={paper}>Footer</Paper>
-          </Grid>
-          <Grid item xs={12}>
+          </Grid> */}
+          {/* <Grid item xs={12}>
             <LayoutSwiper layoutType={layoutType} />
             <Switch />
-          </Grid>
+          </Grid> */}
         </Grid>
       </Container>
     </div>
