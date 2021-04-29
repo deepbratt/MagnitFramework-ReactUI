24bfc8ffbd07aab0ec018dffcbb086a84aa2f741
ameera-saleem <ameerasaleem430@gmail.com>
2021-04-29 13:02:38 -0700

    Changes to the header

--- src/Theme/Scrolltrigger.js
+++ src/Theme/Scrolltrigger.js
@@ -3,7 +3,7 @@ import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 const Scrolltrigger = () => {
     const trigger = useScrollTrigger({
         disableHysteresis: true,
-        threshold: 600
+        threshold: 400
       })
       
       return{trigger}
