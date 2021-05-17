7423b31f4d42efad3cae5a78a8810a451221dad4
jamshedahmed19 <jamsheda4ahmed786@gmail.com>
2021-05-17 19:18:51 +0500

    Latest Blog Card Completed

--- src/Pages/OurBlogs/index.js
+++ src/Pages/OurBlogs/index.js
@@ -1,20 +1,23 @@
-import { Typography } from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
 import Banner from "../../Components/Banner";
 import { Colors } from "../../Theme/color.constants";
 import {
   BannerText,
   TrendingBlogsSectionText,
+  LatestBlogsSectionText,
 } from "../../Utils/Constants/Language/en/OurBlogsPageText";
 import BannerImage from "../../assets/OurBlogs/BannerImage.png";
 import TrendingBlogCard from "../../assets/OurBlogs/TrendingBlogCard.png";
 import BreadCrumb from "../../Components/BreadCrumb";
 import Section from "../Section";
 import TrendingBlogs from "./TrendingBlogs";
+import LatestBlogs from "./LatestBlogs";
 import ReviewSlider from "../../Components/ReviewSlider";
+import { Pagination } from "@material-ui/lab";
 
 const OurBlogs = () => {
   const { title, subtitle } = BannerText;
-  const { linearBackground } = Colors;
+  const { linearBackground, BlueRibbon } = Colors;
 
   const breadCrumData = [
     {
@@ -49,7 +52,28 @@ const OurBlogs = () => {
         </Typography>
       </Banner>
       <Section title={header}>
-        <ReviewSlider slides={slideArr} showArrows={false}/>
+        <ReviewSlider slides={slideArr} showArrows={false} />
+      </Section>
+      <Section title={LatestBlogsSectionText.header} backColor={BlueRibbon}>
+        <Grid container>
+          {LatestBlogsSectionText.cards &&
+            LatestBlogsSectionText.cards.map((card, index) => (
+              <Grid style={{ margin: "20px 0" }} key={index} item xs={12} lg={4}>
+                <LatestBlogs cardData={card} />
+              </Grid>
+            ))}
+          <Grid item xs={12}>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                marginTop: "40px",
+              }}
+            >
+              <Pagination count={4} color="primary" />
+            </div>
+          </Grid>
+        </Grid>
       </Section>
     </>
   );
