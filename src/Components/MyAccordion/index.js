7b7e9b0d2f2b0b6efd5fb0abbb906ba50831b66f
ameera-saleem <ameerasaleem430@gmail.com>
2021-05-17 04:20:14 -0700

    Removed unnecessary code from Accordian

--- src/Components/MyAccordion/index.js
+++ src/Components/MyAccordion/index.js
@@ -11,20 +11,11 @@ import Minus from "../../assets/icons/closeIcon.png";
 import Data from "../../Pages/ContactUs/questions";
 
 const MyAccordion = () => {
-  const [payload, setPayload] = useState(Data);
   const [expanded, setExpanded] = useState(false);
   const handleChange = (panel) => (event, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
   };
   const { root } = LayoutStyle();
-
-  const dispatchClick = (index) => {
-    let _shallowCopy = [...payload];
-    let _cItem = { ..._shallowCopy[index] };
-    _cItem["x"] = _cItem["x"] === "+" ? "-" : "+";
-    _shallowCopy[index] = _cItem;
-    setPayload(_shallowCopy);
-  };
 
   const accordionDataController = [
     {
@@ -55,34 +46,6 @@ const MyAccordion = () => {
 
   return (
     <div className={root}>
-      {/* {(
-        payload.map((question, index) => (
-          <Accordion
-            // expanded={…(expanded === "panel" && {expanded: true})}
-            {...(expanded === "panel" && {expanded: true})}
-            onChange={handleChange("panel1")}
-          >
-            <AccordionSummary
-              expandIcon={
-                <div onClick={() => dispatchClick(index)}>
-                  {question.x === "+" ? (
-                    <img src={Plus} />
-                  ) : (
-                    <img src={Minus} />
-                  )}
-                </div>
-              }
-              aria-controls="panel1bh-content"
-              id="panel1bh-header"
-            >
-              <Typography>{question.summary}</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>{question.detail}</Typography>
-            </AccordionDetails>
-          </Accordion>
-        ))
-      )} */}
 
       {accordionDataController.map((item, index) => (
         <Accordion key={'accordion-'+index} expanded={expanded === 'accordion-'+index} onChange={handleChange('accordion-'+index)}>
