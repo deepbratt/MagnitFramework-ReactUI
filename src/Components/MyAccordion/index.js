716c8f4fa97ced9959ef857f8663975e20a9bf07
Abdul Zahir <abdul.zahir@digitalpumpkins.com>
2021-05-17 16:06:42 +0500

    accordion bug fixed

--- src/Components/MyAccordion/index.js
+++ src/Components/MyAccordion/index.js
@@ -8,7 +8,7 @@ import { useState } from "react";
 import CustomButton from "../CustomButton";
 import Plus from "../../assets/icons/expandIcon.png";
 import Minus from "../../assets/icons/closeIcon.png";
-import Data from "../../Pages/ContactUs/questions"
+import Data from "../../Pages/ContactUs/questions";
 
 const MyAccordion = () => {
   const [payload, setPayload] = useState(Data);
@@ -26,9 +26,36 @@ const MyAccordion = () => {
     setPayload(_shallowCopy);
   };
 
+  const accordionDataController = [
+    {
+      ariaControl: "panel1a-content",
+      ariaId: "panel1a-header",
+      summary: Data[0].summary,
+      detail: Data[0].detail,
+    },
+    {
+      ariaControl: "panel2a-content",
+      ariaId: "panel2a-header",
+      summary: Data[1].summary,
+      detail: Data[1].detail,
+    },
+    {
+      ariaControl: "panel3a-content",
+      ariaId: "panel3a-header",
+      summary: Data[2].summary,
+      detail: Data[2].detail,
+    },
+    {
+      ariaControl: "panel4a-content",
+      ariaId: "panel4a-header",
+      summary: Data[3].summary,
+      detail: Data[3].detail,
+    },
+  ];
+
   return (
     <div className={root}>
-      {(
+      {/* {(
         payload.map((question, index) => (
           <Accordion
             // expanded={…(expanded === "panel" && {expanded: true})}
@@ -55,7 +82,22 @@ const MyAccordion = () => {
             </AccordionDetails>
           </Accordion>
         ))
-      )}
+      )} */}
+
+      {accordionDataController.map((item, index) => (
+        <Accordion key={'accordion-'+index} expanded={expanded === 'accordion-'+index} onChange={handleChange('accordion-'+index)}>
+          <AccordionSummary
+            expandIcon={expanded === 'accordion-'+index ? <img src={Minus}/> :<img src={Plus} />}
+            aria-controls={item.ariaControl}
+            id={item.ariaId}
+          >
+            <Typography>{item.summary}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>{item.detail}</Typography>
+          </AccordionDetails>
+        </Accordion>
+      ))}
       <CustomButton text="See More" />
     </div>
   );
