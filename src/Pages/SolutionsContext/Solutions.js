49777db204fcb48bea6b584774b30e429e70fa03
ameera-saleem <ameerasaleem430@gmail.com>
2021-05-17 13:04:22 -0700

    Changed text color to black

--- src/Pages/SolutionsContext/Solutions.js
+++ src/Pages/SolutionsContext/Solutions.js
@@ -13,7 +13,7 @@ const Solutions = () => {
   const classes = useStyles();
   const { root } = classes;
   const {heading,subHeading} = Data
-  const {whiteColor} = Colors
+  const {whiteColor,blackColor} = Colors
   return (
     <>
       <Paper className={root} elevation={0}>
@@ -28,7 +28,7 @@ const Solutions = () => {
         <PrimarySection />
         <MidSection />
         <MuiThemeProvider theme={THEME}>
-        <CustomButton style={{backgroundColor: whiteColor,padding:".3rem 1.9rem"}} variant="contained" size={Breakpoints()} >
+        <CustomButton style={{backgroundColor: whiteColor,padding:".3rem 1.9rem", color: blackColor}} variant="contained" size={Breakpoints()} >
 
           See More
         </CustomButton>
