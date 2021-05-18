46bcad30bc624699c7bc7c6be66ffd60830aa00f
jamshedahmed19 <jamsheda4ahmed786@gmail.com>
2021-05-18 16:21:18 +0500

    Update AboutMainSection.js
    Replaced Paper With Grid

--- src/Sections/MainComponent/AboutMainSection.js
+++ src/Sections/MainComponent/AboutMainSection.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useStyles } from "./mainStyles";
-import { Card, CardContent, Paper } from "@material-ui/core";
+import { Card, CardContent, Grid } from "@material-ui/core";
 import Heading from "../../ContainerStructure/Headings/Heading4";
 import Paragraph from "../../ContainerStructure/Headings/Paragraphs/Paragraph";
 import { MainData } from "../../Utils/Constants/Language/AboutUsData";
@@ -15,31 +15,29 @@ const AboutMainSection = (props) => {
     classes;
   return (
     <>
-      <Paper className={root} elevation={0}>
-        <section>
-          <Card className={card}>
-            <div className={breadCrumbStyles}>
-              <BreadCrumb links={props.breadcrumbData} />
-            </div>
+      <Grid className={root} elevation={0}>
+        <Card className={card}>
+          <div className={breadCrumbStyles}>
+            <BreadCrumb links={props.breadcrumbData} />
+          </div>
+          <CardContent className={cardSec}>
+            <CustomImage className={hero} alt="hero" src={props.hero} />
+          </CardContent>
+        </Card>
+        {props.card === true ? (
+          <Card>
             <CardContent className={cardSec}>
-              <CustomImage className={hero} alt="hero" src={props.hero} />
+              <section className={underlined}>
+                <Heading title={title} />
+              </section>
+              <section className={paragraph}>
+                <Paragraph para={paragraphPrimary} />
+                <Paragraph para={paragraphSecondary} />
+              </section>
             </CardContent>
           </Card>
-          {props.card === true ? (
-            <Card>
-              <CardContent className={cardSec}>
-                <section className={underlined}>
-                  <Heading title={title} />
-                </section>
-                <section className={paragraph}>
-                  <Paragraph para={paragraphPrimary} />
-                  <Paragraph para={paragraphSecondary} />
-                </section>
-              </CardContent>
-            </Card>
-          ) : null}
-        </section>
-      </Paper>
+        ) : null}
+      </Grid>
     </>
   );
 };
