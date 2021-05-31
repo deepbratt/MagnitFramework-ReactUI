948c79911f4a3099bd1cc550a6e70197fe4b5792
jamshedahmed19 <jamsheda4ahmed786@gmail.com>
2021-05-31 19:45:25 +0500

    Updated Opportunity context

--- src/Pages/Careers/OpportunityContext.js
+++ src/Pages/Careers/OpportunityContext.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { useStyles } from "./careerStyles";
-import { Grid, Typography } from "@material-ui/core";
-import Heading4 from "../../ContainerStructure/Headings/Heading4";
-import Breakpoints from "../../Theme/theme.breakpoints";
+import { Grid, Typography, Card, Hidden } from "@material-ui/core";
 import { Colors } from "../../Theme/color.constants";
 import {
   DataArray,
@@ -13,81 +11,109 @@ import {
 import CustomButton from "../../Components/CustomButton";
 import ReadMore from "../../Components/ReadMore";
 import CustomTitle from "../Section/CustomTitle";
+import Section from "../Section";
 const OpportunityContext = () => {
   const classes = useStyles();
-  const {
-    root,
-    card,
-    devSec,
-    HeadSec,
-    para,
-    button,
-    RightPattern,
-    LeftPattern,
-    remoteArea,
-    bottomStyle,
-  } = classes;
-  const { whiteColor, Harlequin } = Colors;
+  const { card, RightPattern, LeftPattern, remoteArea } = classes;
+  const { whiteColor, skyBlue } = Colors;
+
+  const ourWorkSectionPatterns = [
+    {
+      image: (
+        <Hidden smDown>
+          <Grid
+            className={leftPattern}
+            style={{ position: "absolute" }}
+            item
+            xs={3}
+          >
+            <img width="100%" height="100%" src={leftPattern} alt="" />
+          </Grid>
+        </Hidden>
+      ),
+      styles: LeftPattern,
+    },
+    {
+      image: (
+        <Hidden smDown>
+          <Grid
+            className={`${rightPattern}`}
+            style={{ position: "absolute" }}
+            item
+            xs={4}
+          >
+            <img width="100%" height="100%" src={rightPattern} alt="" />
+          </Grid>
+        </Hidden>
+      ),
+      styles: RightPattern,
+    },
+  ];
+
   return (
     <>
-      <Grid container className={root} spacing={0} justify="center">
-        <Grid item xs={12}>
-          <img className={RightPattern} alt="" src={rightPattern} />
-          <img className={LeftPattern} alt="" src={leftPattern} />
-          <section>
-          <CustomTitle color={whiteColor} text={heading} underlined={false}/>
-          </section>
+      <Section
+        style={{ position: "relative" }}
+        backColor={skyBlue}
+        patterns={ourWorkSectionPatterns}
+      >
+        <CustomTitle color={whiteColor} text={heading} underlined={false} />
+
+        <Grid
+          style={{ marginBottom: "40px" }}
+          container
+          justify="center"
+          spacing={2}
+        >
+          {/* <section className={cardSec}> */}
+          {DataArray.map((data, index) => {
+            return (
+              <Grid item xs={12} sm={4} lg={3} key={index}>
+                <Card className={card}>
+                  <Grid>
+                    <Grid xs={12}>
+                      <Typography color="textPrimary" variant="h5">
+                        {data.title}
+                      </Typography>
+                    </Grid>
+                    <Grid className={remoteArea}>
+                      <div style={{ marginRight: ".5rem" }}>
+                        <img alt="" width="10px" src={data.icon} />
+                      </div>
+                      <div>
+                        <Typography color="textSecondary" variant="subtitle2">
+                          {data.span}
+                        </Typography>
+                      </div>
+                    </Grid>
+                    <Grid xs={12}>
+                      <Typography
+                        color="textSecondary"
+                        variant="body1"
+                        gutterBottom
+                      >
+                        <ReadMore
+                          text={data.desc}
+                          maxLines={3}
+                          ellipses="... Read More"
+                        />
+                      </Typography>
+                    </Grid>
+                  </Grid>
+
+                  <Grid xs={12}>
+                    <CustomButton>{data.buttonText}</CustomButton>
+                  </Grid>
+                </Card>
+              </Grid>
+            );
+          })}
         </Grid>
-       
-        {/* <section className={cardSec}> */}
-        {DataArray.map((data, index) => {
-          return (
-           
-            <Grid
-              xs={12}
-              sm={4}
-              md={4}
-              lg={3}
-              item
-              className={card}
-              key={index}
-            >
-           
-              <Grid className={devSec}>
-                <Grid xs={12} className={HeadSec}>
-                  <Typography variant="subtitle1">{data.title}</Typography>
-                </Grid>
-                <Grid className={remoteArea}>
-                  <div style={{ marginRight: ".5rem" }}>
-                    <img alt="" width="10px" src={data.icon} />
-                  </div>
-                  <div>
-                    <Typography variant="subtitle2">{data.span}</Typography>
-                  </div>
-                </Grid>
-                <Grid xs={12} className={para}>
-                  <Typography variant="body1" component="body1">
-                    <ReadMore
-                      text={data.desc}
-                      maxLines={3}
-                      ellipses="... Read More"
-                    />
-                  </Typography>
-                </Grid>
-              </Grid>
-            
-              <Grid xs={12} className={bottomStyle}>
-                <CustomButton>{data.buttonText}</CustomButton>
-              </Grid>
-            </Grid>
-          );
-        })}
-       
         {/* </section> */}
         <Grid item xs={12}>
           <CustomButton color="secondary">Learn More</CustomButton>
         </Grid>
-      </Grid>
+      </Section>
     </>
   );
 };
--- src/Pages/Careers/careerStyles.js
+++ src/Pages/Careers/careerStyles.js
@@ -1,102 +1,48 @@
 import { makeStyles } from "@material-ui/core/styles";
 import { Colors } from "../../Theme/color.constants";
 
-const { whiteColor, Harlequin, blackColor, paletteBlue,skyBlue, DoveGray,Mirage,darkSilver } = Colors;
-
+const { DoveGray } = Colors;
 
 export const useStyles = makeStyles((theme) => ({
   root: {
-    margin: "0px",
-    backgroundColor: skyBlue,
-    padding: "50px 0px",
     position: "relative",
-    
-  },
-  cardSec: {
-    display: "flex",
-    width: "100%",
-    flexFlow: "wrap",
-    justifyContent: "center",
-  },
-  devSec: {
-    color: blackColor,
-    display: "flex",
-    flexDirection: "column",
-    paddingTop: "20px",
-   
-  },
-  HeadSec:{
-    "& > subtitle1" : {
-      color:Mirage,
-      fontWeight:600,
-    },
-   
-    "& > subtitle2" : {
-      color:darkSilver,
-fontWeight:"normal"
-    },
-  },
-  para:{
-    margin:".5rem 0rem",
-    cursor: "pointer",
-    "& > body1" : {
-      color:darkSilver,
-fontWeight:"normal"
-    },
   },
   card: {
-    background:"white",
-    height: "auto",
     display: "flex",
     zIndex: 1,
     flexDirection: "column",
     alignItems: "center",
-    justifyContent:"center", 
     transition: "all .25s linear",
     boxShadow: "0px 0px 0px 0px rgba(0,0,0,0)",
-    margin: "20px",
-    padding:"20px",
-    textAlign:"center",
-    borderRadius:"5px"
- 
+    padding: "20px",
+    textAlign: "center",
+    borderRadius: "5px",
   },
-  button: {
-    borderRadius: "none",
-    fontFamily: "Poppins",
-    backgroundColor: paletteBlue,
-    transition: "all .3s linear",
-    color: Harlequin,
-    "&:hover": {
-      backgroundColor: Harlequin,
-      color: whiteColor,
-      transition: "all .3s linear",
-    },
-  },
-  RightPattern:{
+
+  RightPattern: {
     position: "absolute",
     right: "0px",
     top: "0px",
     zIndex: 0,
-    width: "500px"
+    maxWidth: "500px",
   },
-  LeftPattern:{
+  LeftPattern: {
     position: "absolute",
     left: "0px",
     bottom: "0px",
     zIndex: 0,
-    width: "300px"
+    maxWidth: "300px",
   },
-  remoteArea:
-  { color: DoveGray, 
+  remoteArea: {
+    color: DoveGray,
     paddingTop: "10px",
-  display:"flex",
-  alignItems:"baseline",
-justifyContent:"center"
-},
-  bottomStyle:{ 
-    display:"flex",
-  alignItems:"flex-end" ,
-  justifyContent:"center"
- }
-
+    display: "flex",
+    alignItems: "baseline",
+    justifyContent: "center",
+  },
+  bottomStyle: {
+    display: "flex",
+    alignItems: "flex-end",
+    justifyContent: "center",
+  },
 }));
