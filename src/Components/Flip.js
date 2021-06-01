aaed61eb6d456b67edcc10dd76af43988253ef21
jamshedahmed19 <jamsheda4ahmed786@gmail.com>
2021-06-01 22:33:50 +0500

    updated icon

--- src/Components/Flip.js
+++ src/Components/Flip.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { useStyles } from "../Sections/HomePageSections/ServicesContext/servicesStyles";
-import { Typography, CardContent, Grid, Card } from "@material-ui/core";
+import { Typography, Grid } from "@material-ui/core";
 import CustomImage from "./CustomImage";
-import ReadMore from "./ReadMore";
 import { NavLink } from "react-router-dom";
 import ReactCardFlip from "react-card-flip";
 import PropTypes from "prop-types";
@@ -28,79 +27,84 @@ const Flip = ({ data }) => {
   //   Use this Card for Services Section pass data from its Parent Contanier to here
   return (
     <>
-    <div  onMouseEnter={() => setIsFlipped(true)}  onMouseLeave={() => setIsFlipped(false)}>
-      <ReactCardFlip
-        isFlipped={isFlipped}
-        className={vertical}
-        flipDirection="horizontal"
+      <div
+        onMouseEnter={() => setIsFlipped(true)}
+        onMouseLeave={() => setIsFlipped(false)}
       >
-        <Grid
-          container
-          className={VertSec}
-          onMouseEnter={() => setIsFlipped(true)}
+        <ReactCardFlip
+          isFlipped={isFlipped}
+          className={vertical}
+          flipDirection="horizontal"
         >
           <Grid
-            item={12}
-            style={{ backgroundColor: color }}
-            className={supportSub}
+            container
+            className={VertSec}
+            onMouseEnter={() => setIsFlipped(true)}
           >
-         
-            <CustomImage
-              alt="icon"
-              width="50px"
-              height="50px"
-              src={icon}
-              style={{ filter: fill }}
-              className={badge}
-            />
-          </Grid>
-          <Grid item={12}>
-            <Typography
-              color="textPrimary"
-              variant="subtitle1"
-              className={name}
-              gutterBottom
+            <Grid
+              item={12}
+              style={{ backgroundColor: color }}
+              className={supportSub}
             >
-              {title}
-            </Typography>
-          </Grid>
-        </Grid>
-        <Grid
-          container
-          className={devSec}
-          onMouseLeave={() => setIsFlipped(false)}
-        >
-          <Grid item={12} classNamw={contentRight}>
-            <Typography
-              color="textPrimary"
-              variant="subtitle1"
-              className={name}
-              gutterBottom
-            >
-              {title}
-            </Typography>
-
-            <Typography variant="subtitle2" className={detail} paragraph={true}>
-              {desc}
-            </Typography>
-          </Grid>
-          <Grid item={12}>
-            <CustomButton size="small">
-              {" "}
-              <NavLink
-                to={redirect}
-                style={{
-                  marginLeft: "10px",
-                  textDecoration: "none",
-                  color: whiteColor,
-                }}
+              <CustomImage
+                alt="icon"
+                width="50px"
+                height="50px"
+                src={icon}
+                style={{ filter: fill }}
+                className={badge}
+              />
+            </Grid>
+            <Grid item={12}>
+              <Typography
+                color="textPrimary"
+                variant="subtitle1"
+                className={name}
+                gutterBottom
               >
-                Read More
-              </NavLink>
-            </CustomButton>
+                {title}
+              </Typography>
+            </Grid>
           </Grid>
-        </Grid>
-      </ReactCardFlip>
+          <Grid
+            container
+            className={devSec}
+            onMouseLeave={() => setIsFlipped(false)}
+          >
+            <Grid item={12} classNamw={contentRight}>
+              <Typography
+                color="textPrimary"
+                variant="subtitle1"
+                className={name}
+                gutterBottom
+              >
+                {title}
+              </Typography>
+
+              <Typography
+                variant="subtitle2"
+                className={detail}
+                paragraph={true}
+              >
+                {desc}
+              </Typography>
+            </Grid>
+            <Grid item={12}>
+              <CustomButton size="small">
+                <NavLink
+                  to={redirect}
+                  style={{
+                    marginLeft: "10px",
+                    textDecoration: "none",
+                    color: whiteColor,
+                  }}
+                >
+                  Read More
+                </NavLink>
+              </CustomButton>
+            </Grid>
+          </Grid>
+        </ReactCardFlip>
       </div>
     </>
   );
