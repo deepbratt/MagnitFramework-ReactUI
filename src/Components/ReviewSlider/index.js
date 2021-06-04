dd8af916081e6d2d9283e1beecb46703f1ad6e28
Abdul Zahir <abdul.zahir@digitalpumpkins.com>
2021-06-04 20:20:18 +0500

    added autoplay

--- src/Components/ReviewSlider/index.js
+++ src/Components/ReviewSlider/index.js
@@ -1,11 +1,11 @@
 import { Carousel } from "react-responsive-carousel";
 import ReviewCard from "./ReviewCard";
 import ReviewSliderStyles from "./style";
-import prev from "../../assets/services/prev.png";
-import next from "../../assets/services/next.png";
+// import prev from "../../assets/services/prev.png";
+// import next from "../../assets/services/next.png";
 import { Grid, useMediaQuery, useTheme } from "@material-ui/core";
 import { reviewSliderText } from "../../Utils/Constants/Language";
-import CustomImage from "../CustomImage";
+// import CustomImage from "../CustomImage";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -14,66 +14,77 @@ const slideArr = reviewSliderText.map((a, index) => {
   return <ReviewCard key={index} cardData={a} />;
 });
 
-const ReviewSlider = ({ slides, showArrows, showDots, indicatorsPosition, itemsPerSlide }) => {
+const ReviewSlider = ({
+  slides,
+  // showArrows,
+  showDots,
+  // indicatorsPosition,
+  itemsPerSlide,
+}) => {
   const visible = !showDots ? "hidden" : "visible";
   const theme = useTheme();
-  const isMDDown = useMediaQuery(theme.breakpoints.up('md'));
-  const indicatorStyles = {
-    background: "rgba(98, 173, 244, 1)",
-    width: 16,
-    height: 16,
-    borderRadius: 25,
-    display: "inline-block",
-    margin: "0px 8px",
-    visibility: visible,
-  };
-  const { nextBtn, prevBtn, carouselRoot, sliderRoot } = ReviewSliderStyles();
-  const customDots = (onClickHandler, isSelected, index, label) => {
-    if (isSelected) {
-      return (
-        <li
-          style={{
-            ...indicatorStyles,
-            background: "#151827",
-            height: 20,
-            width: 20,
-          }}
-          aria-label={`Selected: ${label} ${index + 1}`}
-          title={`Selected: ${label} ${index + 1}`}
-        />
-      );
-    }
+  const isMDUp = useMediaQuery(theme.breakpoints.up("md"));
+  // const indicatorStyles = {
+  //   background: "rgba(98, 173, 244, 1)",
+  //   width: 16,
+  //   height: 16,
+  //   borderRadius: 25,
+  //   display: "inline-block",
+  //   margin: "0px 8px",
+  //   visibility: visible,
+  // };
+  const { sliderRoot } = ReviewSliderStyles();
+  // const customDots = (onClickHandler, isSelected, index, label) => {
+  //   if (isSelected) {
+  //     return (
+  //       <li
+  //         style={{
+  //           ...indicatorStyles,
+  //           background: "#151827",
+  //           height: 20,
+  //           width: 20,
+  //         }}
+  //         aria-label={`Selected: ${label} ${index + 1}`}
+  //         title={`Selected: ${label} ${index + 1}`}
+  //       />
+  //     );
+  //   }
 
-    return (
-      <li
-        style={indicatorStyles}
-        onClick={onClickHandler}
-        onKeyDown={onClickHandler}
-        value={index}
-        key={index}
-        role="button"
-        tabIndex={0}
-        title={`${label} ${index + 1}`}
-        aria-label={`${label} ${index + 1}`}
-      />
-    );
-  };
+  //   return (
+  //     <li
+  //       style={indicatorStyles}
+  //       onClick={onClickHandler}
+  //       onKeyDown={onClickHandler}
+  //       value={index}
+  //       key={index}
+  //       role="button"
+  //       tabIndex={0}
+  //       title={`${label} ${index + 1}`}
+  //       aria-label={`${label} ${index + 1}`}
+  //     />
+  //   );
+  // };
 
   return (
-    <Slider dots={true}
-    infinite={true}
-    speed= {500}
-    slidesToShow= {isMDDown ? itemsPerSlide: 1}
-    slidesToScroll={1} arrows={false} className={sliderRoot}>
-                {slides.map((slide, i) => {
+    <Slider
+      dots={true}
+      infinite={true}
+      speed={500}
+      slidesToShow={isMDUp ? itemsPerSlide : 1}
+      slidesToScroll={1}
+      arrows={false}
+      className={sliderRoot}
+      autoplay
+    >
+      {slides.map((slide, i) => {
         return (
           <Grid
             container
             justify="center"
-            key={i+"2nd"}
+            key={i + "2nd"}
             style={{
               display: "flex",
-              height:"100%"
+              height: "100%",
             }}
           >
             {slide}
@@ -88,7 +99,7 @@ ReviewSlider.defaultProps = {
   slides: slideArr,
   showArrows: true,
   showDots: true,
-  itemsPerSlide:1
+  itemsPerSlide: 1,
 };
 
 export default ReviewSlider;
