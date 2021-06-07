6acf6a63cc5d6cfb9495fcc400fe23eb06c4d48d
Fiza <wfiza842@gamil.com>
2021-06-07 22:42:32 +0500

    lazy and suspense npm loader removed

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,19 @@
-import React, { lazy, Suspense } from "react";
+import React, { lazy } from "react";
 // import { useSelector } from "react-redux";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 import Layout from "./Layout";
 import RequireAuth from "./container/RequireAuth";
 import { pageRoutes } from "./Components/Path";
 import { Loader } from "./Components/loader";
-import { Colors } from "./Theme/color.constants";
-const Home = lazy(() => import("./Pages/Home"));
-const Error = lazy(() => import("./Pages/Error/Index"));
+ import Home from "./Pages/Home";
+  import Error from "./Pages/Error/Index";
 
 const Routes = () => {
   const routeArray = Object.values(pageRoutes);
   // const isLoggedIn = useSelector((state) => state.userReducer.isLoggedin);
-  const { Harlequin } = Colors;
+
   return (
-    <Suspense fallback={<Loader color={Harlequin} styles="h4" />}>
+    
       <Router>
         <Layout>
           <Switch>
@@ -35,7 +34,6 @@ const Routes = () => {
           </Switch>
         </Layout>
       </Router>
-    </Suspense>
   );
 };
 export default Routes;
