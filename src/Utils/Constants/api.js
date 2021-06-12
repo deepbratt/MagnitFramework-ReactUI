e0122dd8e6cf82b19eb2d2fcaa58842d1932401b
Abdul Zahir <abdul.zahir@digitalpumpkins.com>
2021-06-12 18:24:12 +0500

    Base Url updated

--- src/Utils/Constants/api.js
+++ src/Utils/Constants/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const BASE_URL = "http://3.14.6.42/v1/"
+const BASE_URL = "http://3.138.190.235/v1/"
 const axiosInstance = axios.create({
     baseURL:BASE_URL,
     headers:{
