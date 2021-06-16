653a8506bc337e17ae7ca1d70671953036e5cd1f
jamshedahmed19 <jamsheda4ahmed786@gmail.com>
2021-06-16 15:03:38 +0500

    updated images

--- src/Utils/Constants/Language/en/TestimonialVideos.js
+++ src/Utils/Constants/Language/en/TestimonialVideos.js
@@ -1,11 +1,16 @@
-import Avatar1 from "../../../../assets/Testimonials/prof.png";
+import Avatar0 from "../../../../assets/Testimonials/prof.png";
 import HomeDesign from "../../../../assets/Testimonials/homedesign.png";
 import Raleigh from "../../../../assets/Testimonials/raleigh.png";
 import MyNews from "../../../../assets/Testimonials/myNews.png";
 import NailIt from "../../../../assets/Testimonials/nailit.png";
 import SwiftDeals from "../../../../assets/Testimonials/swiftDeals.png";
 import TimeCheck from "../../../../assets/Testimonials/timeCheck.png";
-import Avatar2 from "../../../../assets/Testimonials/profile1.png";
+import Avatar1 from "../../../../assets/Testimonials/avatar1.png";
+import Avatar2 from "../../../../assets/Testimonials/avatar2.png";
+import Avatar3 from "../../../../assets/Testimonials/avatar3.png";
+import Avatar4 from "../../../../assets/Testimonials/avatar4.png";
+import Avatar5 from "../../../../assets/Testimonials/avatar5.png";
+import Avatar6 from "../../../../assets/Testimonials/profile1.png";
 const VideosData = [
   {
     id: 1,
@@ -13,7 +18,7 @@ const VideosData = [
     project: "SwiftDealz",
     para: "They provide efficient, time-bound prototypes keeping all KPIs in mind. We recommend them to anyone looking to make an online footprint.  ",
     Img: SwiftDeals,
-    Avatar: Avatar2,
+    Avatar: Avatar0,
     date: "21 April, 2021",
   },
   {
@@ -22,7 +27,7 @@ const VideosData = [
     project: "Nailit",
     para: "They offer 24/7 support with a smile. They gel up easily with our current team, working for a collaborative solution rather than a compromise. ",
     Img: NailIt,
-    Avatar: Avatar1,
+    Avatar: Avatar6,
     date: "13 May, 2021",
   },
   {
@@ -40,7 +45,7 @@ const VideosData = [
     project: "HomeDesign",
     para: "Magnit offers the best combination of cost and capabilities to assist us with the project. They are quick with milestone deliveries and very adaptable to changing needs and improvements.",
     Img: HomeDesign,
-    Avatar: Avatar2,
+    Avatar: Avatar3,
     date: "10 March, 2021",
   },
   {
@@ -49,7 +54,7 @@ const VideosData = [
     project: "Raleigh Bikes",
     para: "A popular firm selling bicycles needed to overcome the tradition of buying bikes from dealers. So, we created a ‘Design your Bike’ section on their website and marketed it through influencer marketing.",
     Img: Raleigh,
-    Avatar: Avatar2,
+    Avatar: Avatar4,
     date: "28 June, 2021",
   },
   {
@@ -58,7 +63,7 @@ const VideosData = [
     project: "Time Check",
     para: "It is a fantastic experience to work with Magnit. They take time to research your industry and really understand your market before they offer their solutions. They are very attentive to detail and go beyond the call to help you reach your goals.",
     Img: TimeCheck,
-    Avatar: Avatar2,
+    Avatar: Avatar1,
     date: "08 August, 2020",
   },
 ];
