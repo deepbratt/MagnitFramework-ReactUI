4774de3c27572adacafa3bbe8b5f1623ffcd9f67
jamshedahmed19 <jamsheda4ahmed786@gmail.com>
2021-06-30 19:34:38 +0500

    added API in case studies page

--- src/Pages/CaseStudies/index.js
+++ src/Pages/CaseStudies/index.js
@@ -1,11 +1,16 @@
 import { Grid, Hidden } from "@material-ui/core";
-import React, { createRef, useRef } from "react";
+import React, {
+  useState,
+  useEffect,
+  useCallback,
+  createRef,
+  useRef,
+} from "react";
 import { withRouter } from "react-router";
 // import Banner from "../../Components/Banner";
 import Section from "../Section";
 // import useStyles from "./InterActiveSection/useStyles";
-import {useStyles} from "../../Sections/MainComponent/mainStyles"
-import businessPeople from "../../assets/CaseStudies/businessPeople.png";
+import { useStyles } from "../../Sections/MainComponent/mainStyles";
 import { caseStudies } from "../../Utils/Constants/Language";
 import Image1 from "../../assets/images/awardAccredationSection/image 2.png";
 import Image2 from "../../assets/images/awardAccredationSection/image 3.png";
@@ -13,25 +18,33 @@ import Image3 from "../../assets/images/awardAccredationSection/image 4.png";
 import Image4 from "../../assets/images/awardAccredationSection/image 5.png";
 import Image5 from "../../assets/images/awardAccredationSection/image 6.png";
 import CustomImage from "../../Components/CustomImage";
-import {Colors} from '../../Theme/color.constants'
+import { Loader } from "../../Components/loader";
+import MetaTags from "../../Components/MetaTags";
+import { Colors } from "../../Theme/color.constants";
 import AboutMainSection from "../../Sections/MainComponent/AboutMainSection";
-import ResponsiveInterActiveSection from '../../Sections/ResponsiveInterActiveSection/ResponsiveInterActiveSection'
-import InterActiveSection from '../../Sections/InterActiveSection/InterActiveSection'
-import FaqNonInteractive from '../../Sections/FaqNonInteractive/FaqNonInteractive'
-import CustomTitle from "../Section/CustomTitle"
+import ResponsiveInterActiveSection from "../../Sections/ResponsiveInterActiveSection/ResponsiveInterActiveSection";
+import InterActiveSection from "../../Sections/InterActiveSection/InterActiveSection";
+import FaqNonInteractive from "../../Sections/FaqNonInteractive/FaqNonInteractive";
+import CustomTitle from "../Section/CustomTitle";
+import { getPageDataApi } from "../../Utils/APIs/pagesApi";
 
 const CaseStudies = () => {
-  const {img} = useStyles();
+  const [metaData, setMetaData] = useState({});
+  const [sections, setSections] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  const { img } = useStyles();
   const AwardSectionImages = [Image1, Image2, Image3, Image4, Image5];
-  const colors =Colors
-  const faqData = caseStudies.faqs
+  const colors = Colors;
+  const faqData = caseStudies.faqs;
   const arrLength = faqData.length;
   const elRefs = useRef([]);
 
-
   if (elRefs.current.length !== arrLength) {
     // add or remove refs
-    elRefs.current = Array(arrLength).fill().map((_, i) => elRefs.current[i] || createRef());
+    elRefs.current = Array(arrLength)
+      .fill()
+      .map((_, i) => elRefs.current[i] || createRef());
   }
   const breadcrumbData = [
     {
@@ -43,79 +56,146 @@ const CaseStudies = () => {
       text: "Case Studies",
     },
   ];
+
+  const getPageData = useCallback(async () => {
+    setIsLoading(true);
+    await getPageDataApi("case-studies")
+      .then((response) => {
+        if (response.status === "success") {
+          console.log("response", response);
+          setMetaData(response.data.result.metaData);
+          setSections(response.data.result.sections);
+          setIsLoading(false);
+        }
+      })
+      .catch((error) => {
+        console.error("error", error);
+      });
+  }, []);
+
+  useEffect(() => {
+    getPageData();
+    console.log("useEffect");
+  }, [getPageData]);
+
   return (
-    <Grid container>
-      <Grid item xs={12}>
-        {/* <Banner breadCrumb="Home / Case Studies" image={businessPeople} /> */}
-        <AboutMainSection
-          hero={businessPeople}
-          card={false}
-          breadcrumbData={breadcrumbData}
-          width="850px"
-          class={img}
-        />
-      </Grid>
-      <Grid item xs={12}>
-        <Section>
-           <CustomTitle style={{marginBottom: "20px"}} text={caseStudies.heading} underlined={true}/>
-           <CustomTitle subTitle={caseStudies.subHeading}/>
-        </Section>
-      </Grid>
-      <Grid
-        item
-        xs={12}
-        style={{ display: "flex", marginBottom:"150px", marginTop:"100px" }}
-        justify="center"
-      >
-        <Hidden lgUp>
-       
-          <span style={{marginTop:"-280px", marginBottom:"-250px"}}>
-          <Section>
-            <ResponsiveInterActiveSection scrollRef={elRefs} textData={faqData}/>
-            </Section>
-          </span>
-         
-        </Hidden>
-        <Hidden mdDown>
-          <Section>
-            <InterActiveSection scrollRef={elRefs} textData={faqData}/>
-          </Section>
-        </Hidden>
-      </Grid>
-      <Grid item xs={12}>
-        <Section>
-          {/* <FaqNonInteractive toRight/> */}
-          {faqData.map((item, index)=>(
-            <div key={'faq-'+index} style={{paddingTop:"70px"}} ref={elRefs.current[index]}>
-              <FaqNonInteractive toRight={index%2===0 ? false : true} question={item.faqQuestion} answer={item.faqAns}/>
-            </div>
-          ))}
-        </Section>
-      </Grid>
-      <Grid item xs={12}>
-        {/* AWARD AND ACCREDITATIONS SECTION */}
-      <Section>
-      <CustomTitle text={caseStudies.awardsTitle} underlined={true}/>
-        <div
-          style={{
-            display: "flex",
-            flexWrap: "wrap",
-            justifyContent: "space-evenly",
-          }}
-        >
-          {AwardSectionImages &&
-            AwardSectionImages.map((image, index) => (
-              <CustomImage
-                key={index="awards"}
-                style={{ margin: "10px 40px", width: "130px", color: colors.Mirage }}
-                src={image}
-                alt={`client${index}`}
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <Grid container>
+          <MetaTags
+            title={metaData.title}
+            description={metaData.description}
+            canonical={metaData.canonical}
+            keywords={metaData.keywords}
+          />
+
+          <Grid style={{ order: sections.banner.order }} item xs={12}>
+            <AboutMainSection
+              data={sections.banner.dataArray[0]}
+              card={false}
+              breadcrumbData={breadcrumbData}
+              width="850px"
+              class={img}
+            />
+          </Grid>
+          <Grid
+            style={{
+              order: sections.caseStudies.order,
+              display: "flex",
+              marginBottom: "150px",
+              marginTop: "100px",
+            }}
+            container
+            item
+            xs={12}
+            justify="center"
+          >
+            <Section>
+              <CustomTitle
+                style={{ marginBottom: "20px" }}
+                text={sections.caseStudies.title}
+                underlined={true}
               />
-            ))}
-        </div>
-      </Section>
-      </Grid>
-    </Grid>
+              <CustomTitle subTitle={sections.caseStudies.subTitle} />
+            </Section>
+            <Grid
+              style={{
+                display: "flex",
+                marginBottom: "150px",
+                marginTop: "100px",
+              }}
+              item
+              xs={12}
+              justify="center"
+            >
+              <Hidden lgUp>
+                <span style={{ marginTop: "-280px", marginBottom: "-250px" }}>
+                  <Section>
+                    <ResponsiveInterActiveSection
+                      scrollRef={elRefs}
+                      data={sections.caseStudies.dataArray}
+                    />
+                  </Section>
+                </span>
+              </Hidden>
+              <Hidden mdDown>
+                <Section>
+                  <InterActiveSection scrollRef={elRefs} textData={faqData} />
+                </Section>
+              </Hidden>
+            </Grid>
+            <Grid item xs={12}>
+              <Section>
+                {/* <FaqNonInteractive toRight/> */}
+                {sections.caseStudies.dataArray.map((item, index) => (
+                  <div
+                    key={"faq-" + index}
+                    style={{ paddingTop: "70px" }}
+                    ref={elRefs.current[index]}
+                  >
+                    <FaqNonInteractive
+                      toRight={index % 2 === 0 ? false : true}
+                      question={item.title}
+                      answer={item.text}
+                    />
+                  </div>
+                ))}
+              </Section>
+            </Grid>
+          </Grid>
+
+          <Grid style={{ order: sections.awards.order }} item xs={12}>
+            {/* AWARD AND ACCREDITATIONS SECTION */}
+            <Section>
+              <CustomTitle text={caseStudies.awardsTitle} underlined={true} />
+              <div
+                style={{
+                  display: "flex",
+                  flexWrap: "wrap",
+                  justifyContent: "space-evenly",
+                }}
+              >
+                {sections.awards.dataArray &&
+                  sections.awards.dataArray.map((award, index) => (
+                    <CustomImage
+                      key={(index = "awards")}
+                      style={{
+                        margin: "10px 40px",
+                        width: "130px",
+                        color: colors.Mirage,
+                      }}
+                      src={award.image}
+                      alt={`client${index}`}
+                    />
+                  ))}
+              </div>
+            </Section>
+          </Grid>
+        </Grid>
+      )}
+    </>
   );
 };
 
--- src/Sections/ResponsiveInterActiveSection/ResponsiveInterActiveSection.js
+++ src/Sections/ResponsiveInterActiveSection/ResponsiveInterActiveSection.js
@@ -2,31 +2,18 @@ import { Grid } from "@material-ui/core";
 import React from "react";
 import useStyles from "./useStyles";
 import { ReactComponent as MiddleImgBtn } from "../../assets/CaseStudies/middle.svg";
-import { ReactComponent as People } from "../../assets/CaseStudies/people.svg";
-import { ReactComponent as Touch } from "../../assets/CaseStudies/touch.svg";
-import Google from "../../assets/CaseStudies/google.svg";
-import MobileChat from "../../assets/CaseStudies/mobile-chat.svg";
-import MobileApp from "../../assets/CaseStudies/mobile-app.svg";
-import LoveBirds from "../../assets/CaseStudies/love-birds.svg";
 import CircleImageText from "../../Components/CircleImageText";
 import { useHistory } from "react-router";
 
 const ResponsiveInterActiveSection = (props) => {
   const classes = useStyles();
   const history = useHistory();
-  const dataText = props.textData
-  const iconsArray = [
-    <People style={{ width: "40px" }} />,
-    <Touch style={{ width: "40px" }} />,
-    <img src={Google} style={{ width: "40px" }} alt="google" />,
-    <img src={MobileChat} style={{ width: "40px" }} alt="chat"/>,
-    <img src={MobileApp} style={{ width: "40px" }} alt="app"/>,
-    <img src={LoveBirds} style={{ width: "40px" }} alt="dating"/>,
-  ];
+  const { data } = props;
+
   return (
     <Grid
       container
-      style={{ display: "flex"}}
+      style={{ display: "flex" }}
       justify="space-evenly"
       wrap="wrap"
       spacing={0}
@@ -39,16 +26,16 @@ const ResponsiveInterActiveSection = (props) => {
           }}
         />
       </Grid>
-      {dataText.map((item,index)=>(
+      {data.map((item, index) => (
         <CircleImageText
-        key={"responsiveInteractive-"+index}
-        innerImg={iconsArray[index]}
-        text={item.faqQuestion}
-        textRight
-        clickHandler={() => {
-          props.scrollRef.current[index].current.scrollIntoView();
-        }}
-      />
+          key={"responsiveInteractive-" + index}
+          innerImg={item.image}
+          text={item.title}
+          textRight
+          clickHandler={() => {
+            props.scrollRef.current[index].current.scrollIntoView();
+          }}
+        />
       ))}
     </Grid>
   );
