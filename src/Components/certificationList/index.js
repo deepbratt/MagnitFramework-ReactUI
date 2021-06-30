c44b4540f6deb730087ed24291938f0d6ab6860a
ameera-saleem <ameerasaleem430@gmail.com>
2021-06-30 07:33:53 -0700

    Fixed Lottie Image

--- src/Components/certificationList/index.js
+++ src/Components/certificationList/index.js
@@ -14,36 +14,6 @@ const CertificationList = ({ root, data, toRight, mounted }) => {
   const { title, description, jsonFile } = data;
   const [jsonData, setJsonData] = useState()
 
-
-  useEffect(() => {
-    if(mounted && jsonData){
-      setIsMounted(mounted)
-    }
-  }, [mounted, jsonData]);
-
-  useEffect(()=>{
-    getJsonData().then(res=>{
-      setJsonData(res)
-    })
-  },[])
-
-  async function getJsonData(){
-    let jsonResponse = await axios.get(jsonFile)
-    console.log(jsonResponse)
-    return jsonResponse.data
-  }
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: jsonData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
-
-
-
-
   // useEffect(() => {
   //   if(mounted){
   //     setIsMounted(mounted)
@@ -77,23 +47,51 @@ const CertificationList = ({ root, data, toRight, mounted }) => {
   // }
 
 
-  return (
-    <Grid container className={`${root}`}>
-      <Grid item xs={12} md={6}>
-        <div className={`${text} ${toRight ? rightAlignment : leftAlignment}`}>
-          <Typography color="textPrimary" variant="h3">
-            {title}
-          </Typography>
-          <Typography variant="body2" component="p">
-            {description}
-          </Typography>
-        </div>
-      </Grid>
-      <Grid item xs={12} md={6} className={imageWrapper}>
-        {isMounted && <div ref={elem} style={{width:"400px"}}></div>}
-      </Grid>
+
+
+useEffect(() => {
+  if(mounted && jsonData){
+    setIsMounted(mounted)
+  }
+}, [mounted, jsonData]);
+
+useEffect(()=>{
+  getJsonData().then(res=>{
+    setJsonData(res)
+  })
+},[])
+
+async function getJsonData(){
+  let jsonResponse = await axios.get(jsonFile)
+  console.log(jsonResponse)
+  return jsonResponse.data
+}
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: jsonData,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
+};
+
+return (
+  <Grid container className={`${root}`}>
+    <Grid item xs={12} md={6}>
+      <div className={`${text} ${toRight ? rightAlignment : leftAlignment}`}>
+        <Typography color="textPrimary" variant="h3">
+          {title}
+        </Typography>
+        <Typography variant="body2" component="p">
+          {description}
+        </Typography>
+      </div>
     </Grid>
-  );
+    <Grid item xs={12} md={6} className={imageWrapper}>
+      <Lottie options={defaultOptions} width="400px" />
+    </Grid>
+  </Grid>
+);
 };
 
 export default CertificationList;
